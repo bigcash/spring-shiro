3c2bc8137e2127b9c1bfae6243b7f3770883aca0
kate <zhangkaite@126.com>
2016-08-19 14:11:45 +0800

    修正文件上传

--- spring-shiro-training/src/main/java/com/wangzhixuan/controller/bus/ComputerManageController.java
+++ spring-shiro-training/src/main/java/com/wangzhixuan/controller/bus/ComputerManageController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -21,11 +22,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.wangzhixuan.commons.base.BaseController;
 import com.wangzhixuan.commons.utils.PageInfo;
+import com.wangzhixuan.commons.utils.ResponseUtil;
 import com.wangzhixuan.model.bus.ComputerInfo;
 import com.wangzhixuan.service.bus.AbstractService;
 
 /***
  * 十三所二三〇厂涉密内网计算机台账
+ * 
  * @author kate
  *
  */
@@ -33,167 +36,165 @@ import com.wangzhixuan.service.bus.AbstractService;
 @RequestMapping("/computerManage")
 public class ComputerManageController extends BaseController {
 	private static Logger LOGGER = LoggerFactory.getLogger(ComputerManageController.class);
-    @Resource(name="computerManageImpl")
-    private AbstractService computerManageImpl;
+	@Resource(name = "computerManageImpl")
+	private AbstractService computerManageImpl;
 
-    /**
-     * 管理页
-     *
-     * @return
-     */
-    @RequestMapping(value = "/manager", method = RequestMethod.GET)
-    public String manager() {
-        return "bus/computerManage";
-    }
+	/**
+	 * 管理页
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/manager", method = RequestMethod.GET)
+	public String manager() {
+		return "bus/computerManage";
+	}
 
-    /**
-     * 数据列表
-     *
-     * @param userVo
-     * @param page
-     * @param rows
-     * @param sort
-     * @param order
-     * @return
-     */
-    @RequestMapping(value = "/dataGrid", method = RequestMethod.POST)
-    @ResponseBody
-    public Object dataGrid(ComputerInfo computerInfo, Integer page, Integer rows, String sort, String order) {
-        PageInfo pageInfo = new PageInfo(page, rows);
-        Map<String, Object> condition = new HashMap<String, Object>();
-        if (StringUtils.isNoneBlank(computerInfo.getDepname())) {
-            condition.put("depname", computerInfo.getDepname());
-        }
-        if (StringUtils.isNoneBlank(computerInfo.getResperson())) {
-            condition.put("resperson", computerInfo.getResperson());
-        }
-        if (StringUtils.isNoneBlank(computerInfo.getDevno())) {
-            condition.put("devno", computerInfo.getDevno());
-        }
-        pageInfo.setCondition(condition);
-        try {
+	/**
+	 * 数据列表
+	 *
+	 * @param userVo
+	 * @param page
+	 * @param rows
+	 * @param sort
+	 * @param order
+	 * @return
+	 */
+	@RequestMapping(value = "/dataGrid", method = RequestMethod.POST)
+	@ResponseBody
+	public Object dataGrid(ComputerInfo computerInfo, Integer page, Integer rows, String sort, String order) {
+		PageInfo pageInfo = new PageInfo(page, rows);
+		Map<String, Object> condition = new HashMap<String, Object>();
+		if (StringUtils.isNoneBlank(computerInfo.getDepname())) {
+			condition.put("depname", computerInfo.getDepname());
+		}
+		if (StringUtils.isNoneBlank(computerInfo.getResperson())) {
+			condition.put("resperson", computerInfo.getResperson());
+		}
+		if (StringUtils.isNoneBlank(computerInfo.getDevno())) {
+			condition.put("devno", computerInfo.getDevno());
+		}
+		pageInfo.setCondition(condition);
+		try {
 			computerManageImpl.findDataGrid(pageInfo);
 		} catch (Exception e) {
-			LOGGER.error("十三所二三〇厂涉密内网计算机台账分页查询失败,失败的原因是:",e);
+			LOGGER.error("十三所二三〇厂涉密内网计算机台账分页查询失败,失败的原因是:", e);
 		}
-        return pageInfo;
-    }
+		return pageInfo;
+	}
 
-    /**
-     * 添加用户页
-     *
-     * @return
-     */
-    @RequestMapping(value = "/addPage", method = RequestMethod.GET)
-    public String addPage() {
-        return "bus/computerManageAdd";
-    }
+	/**
+	 * 添加用户页
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/addPage", method = RequestMethod.GET)
+	public String addPage() {
+		return "bus/computerManageAdd";
+	}
 
-    /**
-     * 添加数据
-     *
-     * @param userVo
-     * @return
-     */
-    @SuppressWarnings("unchecked")
+	/**
+	 * 添加数据
+	 *
+	 * @param userVo
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-    @ResponseBody
-    public Object add(ComputerInfo computerInfo) {
-    	try {
+	@ResponseBody
+	public Object add(ComputerInfo computerInfo) {
+		try {
 			computerManageImpl.addEntity(computerInfo);
 		} catch (Exception e) {
-			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据添加失败,失败的原因是:",e);
+			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据添加失败,失败的原因是:", e);
 		}
-        return renderSuccess("添加成功");
-    }
+		return renderSuccess("添加成功");
+	}
 
-    /**
-     * 编辑数据
-     *
-     * @param id
-     * @param model
-     * @return
-     */
-    @RequestMapping("/editPage")
-    public String editPage(String id, Model model) {
-    	ComputerInfo computerInfo;
+	/**
+	 * 编辑数据
+	 *
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/editPage")
+	public String editPage(String id, Model model) {
+		ComputerInfo computerInfo;
 		try {
 			computerInfo = (ComputerInfo) computerManageImpl.findById(id);
 			model.addAttribute("computerInfo", computerInfo);
 		} catch (Exception e) {
-			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据根据ID查询失败，失败的原因是:",e);
+			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据根据ID查询失败，失败的原因是:", e);
 		}
-        return "bus/computerManageEdit";
-    }
+		return "bus/computerManageEdit";
+	}
 
-    /**
-     * 更新数据
-     *
-     * @param userVo
-     * @return
-     */
-    @RequestMapping("/edit")
-    @ResponseBody
-    public Object edit(ComputerInfo computerInfo) {
-       
-    	try {
+	/**
+	 * 更新数据
+	 *
+	 * @param userVo
+	 * @return
+	 */
+	@RequestMapping("/edit")
+	@ResponseBody
+	public Object edit(ComputerInfo computerInfo) {
+
+		try {
 			computerManageImpl.updateEntity(computerInfo);
 		} catch (Exception e) {
-			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据根据更新失败，失败的原因是:",e);
+			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据根据更新失败，失败的原因是:", e);
 		}
-        return renderSuccess("修改成功！");
-    }
+		return renderSuccess("修改成功！");
+	}
 
-   
-    /**
-     * 删除数据
-     *
-     * @param id
-     * @return
-     */
-    @RequestMapping("/delete")
-    @ResponseBody
-    public Object delete(String id) {
-    	try {
+	/**
+	 * 删除数据
+	 *
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	@ResponseBody
+	public Object delete(String id) {
+		try {
 			computerManageImpl.deleteById(id);
 		} catch (Exception e) {
-			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据删除失败，失败的原因是:",e);
+			LOGGER.error("十三所二三〇厂涉密内网计算机台账数据删除失败，失败的原因是:", e);
 		}
-        return renderSuccess("删除成功！");
-    }
-    
-    
-    @RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
-    public String fileUpload() {
-        return "bus/computerFileUpload";
-    }
-    
-    
-    
-    @RequestMapping(value = "/upload")  
-    @ResponseBody
-    public Object upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request, ModelMap model) {  
-        String path = request.getSession().getServletContext().getRealPath("upload");  
-        String fileName = file.getOriginalFilename();  
-        File targetFile = new File(path, fileName);  
-        if(!targetFile.exists()){  
-            targetFile.mkdirs();  
-        }  
-        //保存  
-        try {  
-            file.transferTo(targetFile);  
-        } catch (Exception e) {  
-            e.printStackTrace();
-            return renderError("上传失败");
-        }  
-        return renderSuccess("上传成功");
-    }  
-    
-    
-    
-    
-    
-    
-    
-    
+		return renderSuccess("删除成功！");
+	}
+
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
+	public String fileUpload() {
+		return "bus/computerFileUpload";
+	}
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	@ResponseBody
+	public void upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request,
+			HttpServletResponse response, ModelMap model) {
+		String path = request.getSession().getServletContext().getRealPath("upload");
+		String fileName = file.getOriginalFilename();
+		File targetFile = new File(path, fileName);
+		if (!targetFile.exists()) {
+			targetFile.mkdirs();
+		}
+		Map resultMap = new HashMap();
+		// 保存
+		try {
+			file.transferTo(targetFile);
+		} catch (Exception e) {
+			logger.error("文件上传失败,失败的原因是:");
+			resultMap.put("code", "-1");
+		}
+		resultMap.put("code", "1");
+		try {
+			ResponseUtil.WriteJson("upload", resultMap, request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		;
+	}
+
 }
--- spring-shiro-training/src/main/webapp/WEB-INF/views/bus/computerFileUpload.jsp
+++ spring-shiro-training/src/main/webapp/WEB-INF/views/bus/computerFileUpload.jsp
@@ -1,20 +1,46 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="/commons/global.jsp"%>
-<html>
-<head>
-<%@ include file="/commons/basejs.jsp"%>
-<meta http-equiv="X-UA-Compatible" content="edge" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
+<script type="text/javascript" src="${staticPath }/static/easyui/ajaxfileupload.js" charset="utf-8"></script>
 <script type="text/javascript">
+	function ajaxFileUpload() {
+		$.ajaxFileUpload({
+			url : '${path }/computerManage/upload', //需要链接到服务器地址
+			secureuri : false,
+			fileElementId : 'file', //文件选择框的id属性
+			dataType : 'json', //服务器返回的格式类型
+			success : function(data, status) //成功
+			{
+				alert("上传成功！");
+				/* var json = eval("(" + data + ")");//解析返回的json
+				var code = json.code;
+				alert("json:"+json);
+				alert("code:"+code);
+				if (code == 1) {
+					alert("上传成功！");
+				} else {
+					alert("上传失败！");
+				} */
 
+			},
+			error : function(data, status, e) //异常
+			{
+				alert("出错了，请重新上传！");
+			}
+		}
+
+		);
+		return false;
+	}
 </script>
-
-<body>
-	<form id="uploadForm" >
-		<input id="file" type="file" size="45" name="file"  class="easyui-validatebox" data-options="required:true" />
-		<button class="button" id="buttonUpload" onclick="fileUpload();">上传</button>
-	</form>
-</body>
-</html>
+<div>
+	<table>
+		<tr height="50px">
+			<td>
+				<form method="post" enctype="multipart/form-data">
+					<input type="file" id="file" name="file" /> <a href="javascript:void(0)" onclick="ajaxFileUpload()"
+						class="easyui-linkbutton" iconCls="icon-ok">上传</a>
+				</form>
+			</td>
+		</tr>
+	</table>
+</div>
